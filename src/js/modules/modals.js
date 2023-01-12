db1771acef550d9bd45f176b27994af92a451517
Daniil <dvinakovich@gmail.com>
2023-01-12 22:57:00 +0700

    Fix modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,17 +1,29 @@
 function modals() {
   const body = document.body
 
-  function bindModal(btnSelector, modalSelector, closeSelector) {
+  function bindModal(btnSelector, modalSelector, closeSelector, closeClickOverlay = true) {
     const btn = document.querySelectorAll(btnSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector)
+      close = document.querySelector(closeSelector),
+      windows = document.querySelectorAll('[data-modal]')
+
+    function windowsHide() {
+      windows.forEach(window => {
+        window.classList.remove('show')
+        window.classList.add('hide')
+      })
+    }
 
     function showModal(element) {
+      windowsHide()
+
       element.classList.add('show')
       body.classList.add('modal-open')
     }
 
     function hideModal(element) {
+      windowsHide()
+
       element.classList.remove('show')
       body.classList.remove('modal-open')
     }
@@ -28,7 +40,7 @@ function modals() {
     })
 
     modal.addEventListener('click', event => {
-      if (event.target && event.target == modal) {
+      if (event.target && event.target == modal && closeClickOverlay) {
         hideModal(modal)
       }
     })
@@ -50,6 +62,8 @@ function modals() {
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer_close')
   bindModal('.phone_link', '.popup', '.popup .popup_close')
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close')
+  bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false)
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false)
   // showModalByTime('.popup', 60000)
 }
 
